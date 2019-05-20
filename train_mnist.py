309337cc11ad51dd101abd1e72a6c2739847403c
falkaer <kfomosegaard@live.dk>
2019-05-20 23:11:05 +0200

    make sampling code, other random changes

--- train_mnist.py
+++ train_mnist.py
@@ -6,13 +6,13 @@ np.seterr(all='warn')
 from torchvision.datasets import MNIST
 
 from cv import KFold
-#import vb4kde as kde
-import em4kde_torch as kde
+import vb4kde as kde
+# import em4kde_torch as kde
 
 dataset = MNIST('mnist', train=True, download=True)
 mask = torch.cat(((dataset.targets == 8), torch.zeros(10000, dtype=torch.uint8)))
 
-pca_data = torch.load('mnist_pca.pt')
+pca_data = torch.load('mnist_pca_90.pt')
 X_reduced = pca_data['X_reduced'][mask].float().cuda()
 
 N, D = X_reduced.shape
@@ -21,4 +21,4 @@ print(N, D)
 model = kde.KDE(X_reduced, KFold(N, 10))
 kde.train(model, 50)
 
-model.save_kde('kde_em_8.pt')
+model.save_kde('vb_em_8_90.pt')
